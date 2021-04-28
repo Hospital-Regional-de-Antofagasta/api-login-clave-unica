9e8f9f0546d8ee702e3add81fd50fe6df0fdffe4
Pablo Fontecilla <pafonbu@gmail.com>
2021-04-28 10:44:41 -0400

    HRAP-35 CHANGE: obtener ip intento 2

--- api/index.js
+++ api/index.js
@@ -8,13 +8,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-// app.set('trust proxy')
-// app.set('trust proxy', '127.0.0.1')
-// app.set('trust proxy', function (ip) {
-//     if (ip === '127.0.0.1' || ip === '123.123.123.123') return true // trusted IPs
-//     else return false
-//   })
-
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true })
 
 loadConfig()
