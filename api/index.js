ecded6b4637923b23db6f2b84a36da06d0b80324
Pablo Fontecilla <pafonbu@gmail.com>
2021-05-03 16:05:34 -0400

    HRAP-113 CHANGE: hola mundo 4

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 loadConfig()
 
 app.use('/hola', (req, res) => {
-    res.send('Hola Mundo!!')
+    res.send('Hola Mundo!!!!')
 })
 
 app.use('/hra/auth', auth)
