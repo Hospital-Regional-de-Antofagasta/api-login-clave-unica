ad02408ad9b4586e7615d7f91767a1baf363c734
Pablo Fontecilla <pafonbu@gmail.com>
2021-05-10 10:19:56 -0400

    sprint-4 FIX: nombre coleccion del config_login

--- api/models/ConfigApiLogin.js
+++ api/models/ConfigApiLogin.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const ConfigApiLogin = mongoose.model('config_api_login', new Schema ({
+const ConfigApiLogin = mongoose.model(
+  "config_api_login",
+  new Schema({
     mensajes: {
-        unauthorized: String,
-        unauthorizedRefresh: String,
-        serverError: String,
-    }
-}))
+      unauthorized: String,
+      unauthorizedRefresh: String,
+      serverError: String,
+    },
+  }),
+  { collection: "config_api_login" }
+);
 
-module.exports = ConfigApiLogin
+module.exports = ConfigApiLogin;
