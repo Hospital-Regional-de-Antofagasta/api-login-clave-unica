d45e5b7f56c32314a0c9748c2f708572de9fce3a
Pablo Fontecilla <pafonbu@gmail.com>
2021-05-28 09:28:41 -0400

    HRAP-119 ADD: endpoint de testing login

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,6 +3,8 @@ const authController = require("../controller/authController");
 
 const router = express.Router();
 
+router.get("/", authController.loginTest);
+
 router.post("/refresh_token", authController.refreshToken);
 
 module.exports = router;
