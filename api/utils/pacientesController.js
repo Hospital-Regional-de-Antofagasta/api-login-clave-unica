aa5eed7064938c71b89c940328418e757346ca27
Pablo Fontecilla <pafonbu@gmail.com>
2022-02-09 12:10:12 -0300

    ADD: variable ambiente rul de las apis

--- api/utils/pacientesController.js
+++ api/utils/pacientesController.js
@@ -1,39 +1,13 @@
 const axios = require("axios");
 const { signToken } = require("../utils/auth");
-const fakeGetPacientesById = require("../../tests/apiResponses/getPacienteById.json")
-const fakeGetPacientesByRut = require("../../tests/apiResponses/getPacienteByRut.json")
+const fakeGetPacientesById = require("../../tests/apiResponses/getPacienteById.json");
+const fakeGetPacientesByRut = require("../../tests/apiResponses/getPacienteByRut.json");
 
 const secretToken = process.env.JWT_SECRET;
 
 const expiresIn = 60 * 1 * 1 * 1;
 
-// const urlPacientes = process.env.URL_PACIENTE;
-const urlPacientes = "http://localhost:3000";
-
-// exports.getPacienteById = async (idPaciente) => {
-//   const token = signToken(
-//     { _id: idPaciente, rut: null },
-//     expiresIn,
-//     secretToken
-//   );
-
-//   const config = {
-//     headers: {
-//       Authorization: `${token}`,
-//     },
-//   };
-
-//   if (process.env.NODE_ENV === "test") {
-//     return fakeGetPacientesById;
-//   }
-
-//   const response = await axios.get(
-//     `${urlPacientes}/v1/pacientes/informacion`,
-//     config
-//   );
-
-//   return response.data;
-// };
+const urlPacientes = process.env.API_URL;
 
 exports.getPacienteByRut = async (rutPaciente) => {
   const token = signToken(
