ac85b4264f9304701c033f3c0380b432ad08d527
pafonbu <pafonbu@gmail.com>
2022-06-08 16:09:08 -0400

    audit fix

--- api/models/RefreshToken.js
+++ api/models/RefreshToken.js
@@ -3,15 +3,18 @@ const Schema = mongoose.Schema;
 
 const RefreshToken = mongoose.model(
   "refresh_token",
-  new Schema({
-    rutPaciente: { type: String, required: true },
-    key: String,
-    created: { type: Date, default: Date.now },
-    createdByIp: String,
-    revoked: Date,
-    revokedByIp: String,
-    replacedBykey: String,
-  })
+  new Schema(
+    {
+      rutPaciente: { type: String, required: true },
+      key: String,
+      created: { type: Date, default: Date.now },
+      createdByIp: String,
+      revoked: Date,
+      revokedByIp: String,
+      replacedBykey: String,
+    },
+    { timestamps: true }
+  )
 );
 
 module.exports = RefreshToken;
